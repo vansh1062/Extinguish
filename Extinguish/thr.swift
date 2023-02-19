730585d20a1a7c7c749eb5da4523d6aa3c20fdf5
Thrishalini <trishalini973@gmail.com>
2023-02-19 19:57:01 +0530

    edited

--- Extinguish/thr.swift
+++
@@ -1,8 +0,0 @@
-//
-//  thr.swift
-//  Extinguish
-//
-//  Created by Thrishalini on 19/02/23.
-//
-
-import Foundation
--- Extinguish/thr2.swift
+++
@@ -1,8 +0,0 @@
-//
-//  thr2.swift
-//  Extinguish
-//
-//  Created by Thrishalini on 19/02/23.
-//
-
-import Foundation
