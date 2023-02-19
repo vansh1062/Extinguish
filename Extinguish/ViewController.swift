b5b4cde9f33f65000c63a01c2c38865891bed151
Vansh Agarwal <vansh1062@gmail.com>
2023-02-19 19:32:44 +0530

    Initial Commit

---
+++ Extinguish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Extinguish
+//
+//  Created by vansh agarwal on 19/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
