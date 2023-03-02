5c78d278747d60ab95f2f6e661b0e2498330a9c6
Tanvi129 <tanvigupta129@gmail.com>
2023-03-02 10:43:38 +0530

    Floating Action Button Added

--- Extinguish/HRViewController.swift
+++ Extinguish/HRViewController.swift
@@ -29,6 +29,7 @@ class HRViewController: UIViewController , UITableViewDataSource , UITableViewDe
         view.backgroundColor = UIColor(rgb: 0xFFFBEB)
         self.auditorListTable.dataSource = self
         self.auditorListTable.delegate = self
+        view.addSubview(floatingButton)
 
         // Do any additional setup after loading the view.
     }
@@ -73,5 +74,25 @@ class HRViewController: UIViewController , UITableViewDataSource , UITableViewDe
         // Pass the selected object to the new view controller.
     }
     */
+    private let floatingButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+//        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 30
+        button.backgroundColor = UIColor(rgb: 0x0A2647)
+        let image = UIImage(systemName: "plus" , withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium))
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.setTitleColor(.white, for: .normal)
+        button.layer.shadowRadius = 10
+        button.layer.shadowOpacity = 0.3
+        return button
+    }()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        floatingButton.frame = CGRect(x: view.frame.size.width - 70, y: view.frame.size.height - 150 , width: 60, height: 60 )
+        
+    }
+    
 
 }
