4a5d2a2502a57bfa180e36031e1b995e57040ae3
Tanvi129 <tanvigupta129@gmail.com>
2023-03-02 11:13:55 +0530

    commit

---
+++ Extinguish/TaskSummaryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskSummaryTableViewCell.swift
+//  Extinguish
+//
+//  Created by Tanvi Gupta on 02/03/23.
+//
+
+import UIKit
+
+class TaskSummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
